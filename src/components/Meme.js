5b40b3dc8dfea9ef9a48eda9e9ea0986db6a1d49
Andres <andresdomlez@gmail.com>
2022-03-23 00:17:45 +0000

    Form and image change

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,20 +3,59 @@ import './Meme.css';
 import memesData from '../data/memesData';
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
-    console.log(url);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top text" />
-        <input type="text" className="form--input" placeholder="Bottom text" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" alt="" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
